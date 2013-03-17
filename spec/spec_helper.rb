70a16df3c82b8cb9ec5255b13bd85b44120f9f76
zakuni <kunio038@gmail.com>
2013-03-17 19:13:59 +0900

    deleted spec

--- spec/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-require 'minitest/autorun'
-require 'minitest/reporters'
-require 'minitest/pride'
-
-require 'machiawase'
-
-MiniTest::Reporters.use! [MiniTest::Reporters::DefaultReporter.new, MiniTest::Reporters::GuardReporter.new]
