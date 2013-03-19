9afb79b7c640c18d9a050a281a68d0480f2d01d1
zakuni <kunio038@gmail.com>
2013-03-19 23:14:15 +0900

    migrate TestRendezvous to test_rendezvous.rb

--- test/test_machiawase.rb
+++ test/test_machiawase.rb
@@ -7,38 +7,3 @@ class TestMachiawase < MiniTest::Unit::TestCase
     assert_kind_of Machiawase::Place, Machiawase.where("横浜", "東京")
   end
 end
-
-class TestRendezvous < MiniTest::Unit::TestCase
-  include Machiawase
-
-  def setup
-    @rendezvous = Rendezvous.new
-  end
-
-  def test_centroid
-    place_a = Place.new(0, 0)
-    place_b = Place.new(1, 1)
-    assert_equal [0.5, 0.5], @rendezvous.send(:centroid, *[place_a, place_b])
-
-    place_a = Place.new(0, 0)
-    place_b = Place.new(1, 1)
-    place_c = Machiawase::Place.new(2, 2)
-    assert_equal [1, 1], @rendezvous.send(:centroid, *[place_a, place_b, place_c])
-
-    place_a = Place.new(0, 0)
-    place_b = Place.new(2, 0)
-    place_c = Place.new(2, 2)
-    place_d = Place.new(0, 2)
-    assert_equal [1, 1], @rendezvous.send(:centroid, *[place_a, place_b, place_c, place_d])
-  end
-
-  def test_middle_of
-    g1 = Place.geocode("横浜")
-    p1 = Place.new(g1['lat'], g1['lon'])
-        
-    g2 = Place.geocode("東京")
-    p2 = Place.new(g2['lat'], g2['lon'])
-    
-    assert_kind_of Place, @rendezvous.send(:middle_of, p1, p2)
-  end
-end
