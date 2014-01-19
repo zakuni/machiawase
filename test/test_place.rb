5ec33776f769b958f5f5cef5242d36d80e18c3ee
zakuni <kunio038@gmail.com>
2014-01-19 21:37:49 +0900

    configure Geocode
    modify test

--- test/test_place.rb
+++ test/test_place.rb
@@ -6,7 +6,7 @@ class TestPlace < MiniTest::Test
   include Machiawase
   
   def test_address
-    assert_equal "神奈川県横浜市中区真砂町1丁目9", Place.new(35.4437078, 139.6380256).address
+    assert_equal "日本, 神奈川県横浜市中区真砂町１丁目１", Place.new(35.4437078, 139.6380256).address
   end
 
   def test_geocode
