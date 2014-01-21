c29e779f25e3e8ddcb294e3add648619032ad6ff
zakuni <kunio038@gmail.com>
2014-01-22 00:04:28 +0900

    modify parse_proxy as a module method

--- test/test_machiawase.rb
+++ test/test_machiawase.rb
@@ -6,4 +6,28 @@ class TestMachiawase < MiniTest::Test
   def test_where
     assert_kind_of Machiawase::Place, Machiawase.where("横浜", "東京")
   end
+
+  def test_parse_proxy
+    assert_respond_to Machiawase, :parse_proxy
+
+    return_with_nil = Machiawase.parse_proxy(nil)
+    assert_kind_of OpenStruct, return_with_nil
+    assert_respond_to return_with_nil, :user
+    assert_respond_to return_with_nil, :pass
+
+    assert_equal(OpenStruct.new({"server" => "http://host:port",
+                                  "user" => "user",
+                                  "pass" => "pass"}),
+                 Machiawase.parse_proxy("user:pass@host:port"))
+    
+    assert_equal(OpenStruct.new({"server" => "http://host:port",
+                                  "user"   => "",
+                                  "pass"   => ""}),
+                 Machiawase.parse_proxy("host:port"))
+
+    assert_equal(OpenStruct.new({"server" => nil,
+                                  "user"   => "",
+                                  "pass"   => ""}),
+                 Machiawase.parse_proxy(nil))
+  end  
 end
--- test/test_place.rb
+++ test/test_place.rb
@@ -16,28 +16,4 @@ class TestPlace < MiniTest::Test
   def test_near_station
     assert_equal "関内駅", Place.new(35.4437978, 139.6380256).near_station
   end
-
-  def test_parse_proxy
-    assert_respond_to Place, :parse_proxy
-
-    return_with_nil = Place.parse_proxy(nil)
-    assert_kind_of OpenStruct, return_with_nil
-    assert_respond_to return_with_nil, :user
-    assert_respond_to return_with_nil, :pass
-
-    assert_equal(OpenStruct.new({"server" => "http://host:port",
-                                  "user" => "user",
-                                  "pass" => "pass"}),
-                 Place.parse_proxy("user:pass@host:port"))
-    
-    assert_equal(OpenStruct.new({"server" => "http://host:port",
-                                  "user"   => "",
-                                  "pass"   => ""}),
-                 Place.parse_proxy("host:port"))
-
-    assert_equal(OpenStruct.new({"server" => nil,
-                                  "user"   => "",
-                                  "pass"   => ""}),
-                 Place.parse_proxy(nil))
-  end
 end
